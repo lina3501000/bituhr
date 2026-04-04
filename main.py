156bbe3d2d3e69132aac35a525a07e377b9578f1
Helena <tech@helena-heil.de>
2026-04-04 19:08:03 +0200

    potentiometer für alle fabren hinzugefügt

--- main.py
+++ main.py
@@ -2,19 +2,22 @@
 
 
 # Bibliotheken laden
-from machine import Pin
+from machine import Pin, ADC, PWM
 from neopixel import NeoPixel
 import ds3231
-import time
-rtc = ds3231.RTC(sda_pin=20, scl_pin=21)
+from time import sleep
+
+# Pin DS3231
+rtc = ds3231.RTC(sda_pin=16, scl_pin=17, port=0)
 # GPIO-Pin für WS2812
-pin_hour = 28
-pin_min=27
-pin_sec=22
+pin_hour = 20
+pin_min=19
+pin_sec=18
+# Pin potentiometer
+adc0 = ADC(26)
 
 # Anzahl der LEDs
 leds = 6
-
 # Helligkeit: 0 bis 255
 brightness = 10
 
@@ -25,12 +28,9 @@ np_sec = NeoPixel(Pin(pin_sec, Pin.OUT), leds)
 
 print(rtc.ReadTime("everything_sorted"))
 print(rtc.ReadTime("everything_number"))
+# 0 bis 65600
 
 while True:
-    np_hour.write()
-    np_min.write()
-    np_sec.write()
-    
     rtc_hour =int(rtc.ReadTime("hour"))
     print("hour", rtc_hour)
     hour_bit=bin(rtc_hour)
@@ -45,7 +45,12 @@ while True:
     print("sec", rtc_sec)
     sec_bit=bin(rtc_sec)
     print("secbit", sec_bit)
-
+    
+    value = adc0.read_u16()
+    print(value)
+    brightness = min(max(value//256, 0), 255)
+    print(brightness)
+    
     for i in range(6):
         if rtc_hour & 0b1 << i:
             np_hour[i]=(brightness, brightness, brightness)
@@ -67,4 +72,4 @@ while True:
     np_hour.write()
     np_min.write()
     np_sec.write()
-    time.sleep(0.5)
+    sleep(0.1)
