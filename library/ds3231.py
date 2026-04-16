0431b6cd68b072d103e8432dee12ac5d3bf7cd77
Helena <tech@helena-heil.de>
2026-04-16 18:50:57 +0200

    library für zeitumstellung geändert

--- library/ds3231.py
+++ library/ds3231.py
@@ -30,24 +30,43 @@ class RTC:
         except:
             return 'Error: Not connected to DS3231'
         
-        year = self.bcd2bin(buffer[6]) + 2000
+        year = self.bcd2bin(buffer[6])
+        year_buffer=buffer[6]
         month = self.bcd2bin(buffer[5])
+        month_buffer=buffer[5]
         day = self.bcd2bin(buffer[4])
+        day_buffer=buffer[4]
         weekday = self.bcd2bin(buffer[3])
+        weekday_buffer=buffer[3]
         hour = self.bcd2bin(buffer[2])
+        hour_buffer=buffer[2]
         minute = self.bcd2bin(buffer[1])
+        minute_buffer=buffer[1]
         second = self.bcd2bin(buffer[0])
+        second_buffer=buffer[0]
         
+
+        #montag=1, sonntag=7
         # Output
-        if mode == 'DIN-1355-1':
-            return self.pre_zero(day) + '.' + self.pre_zero(month) + '.' + str(year)
-        elif mode == 'DIN-1355-1+time':
-            return self.pre_zero(day) + '.' + self.pre_zero(month) + '.' + str(year) + ' ' + self.pre_zero(hour) + ':' + self.pre_zero(minute) + ':' + self.pre_zero(second)
-        elif mode == 'ISO-8601':
-            return str(year) + '-' + self.pre_zero(month) + '-' + self.pre_zero(day)
-        elif mode == 'time':
-            return self.pre_zero(hour) + ':' + self.pre_zero(minute) + ':' + self.pre_zero(second)
-        elif mode == 'weekday':
-            return self.w[weekday]
+        if mode == 'everything_sorted':
+            return self.pre_zero(hour) + ':' + self.pre_zero(minute) + ':' + self.pre_zero(second) + " " + self.w[weekday] + ": " + self.pre_zero(day) + '.' + self.pre_zero(month) + '.' + str(year)
+        elif mode == 'everything_number':
+            return hour , minute, second , weekday, day, month, year
+        elif mode == "everything_set":
+            return second, minute,  hour , weekday, day, month, year
+        elif mode == "alles_buffer":
+            return second_buffer, minute_buffer, hour_buffer, weekday_buffer, day_buffer, month_buffer,year_buffer
+        elif mode == 'second':
+            return second
+        elif mode == 'minute':
+            return minute
+        elif mode == 'hour':
+            return hour
+        elif mode =="year_kurz":
+            return year_kurz
+        elif mode == "tag":
+            return day
+        elif mode =="year":
+            return year
         else:
-            return second, minute, hour, weekday, day, month, year
+            return "wrong cathegory"
